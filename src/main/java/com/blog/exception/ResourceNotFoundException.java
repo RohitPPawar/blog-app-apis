40b640144ac6f677c288b2cad3c22754e65dd1c8
Rohit Pawar <rohitpawar20121999@gmail.com>
2023-02-28 01:47:47 +0530

    POST controller

---
+++ src/main/java/com/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,34 @@
+package com.blog.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	String resourceName;
+	String filedName;
+	long fieldValue;
+	public ResourceNotFoundException(String resourceName, String filedName, long fieldValue) {
+		super(String.format("%s not found with %s : %l", resourceName,filedName,fieldValue));
+		this.resourceName = resourceName;
+		this.filedName = filedName;
+		this.fieldValue = fieldValue;
+	}
+	public String getResourceName() {
+		return resourceName;
+	}
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+	public String getFiledName() {
+		return filedName;
+	}
+	public void setFiledName(String filedName) {
+		this.filedName = filedName;
+	}
+	public long getFieldValue() {
+		return fieldValue;
+	}
+	public void setFieldValue(long fieldValue) {
+		this.fieldValue = fieldValue;
+	} 
+	
+	
+}
